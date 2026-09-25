d0df46b30328add3501e377c3eb1d2e7ddbbecf5
Subhasish Jena <subhasishjena8280@gmail.com>
2026-09-25 17:08:33 +0530

    code

--- str_formatting.py
+++
@@ -1,3 +0,0 @@
-print(format('i','<10'),format('love','>10'),format('programming','^50'))
-
-#here < for left format , > for right format , ^ for alligin
--- str_slicing.PY
+++
@@ -1,2 +0,0 @@
-str = "hello world"
-print(str)
