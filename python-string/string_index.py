edc2fd2f85365218703f7e91eb1678eadc12139d
Subhasish Jena <subhasishjena8280@gmail.com>
2026-09-25 17:07:35 +0530

    code

---
+++ python-string/string_index.py
@@ -0,0 +1,6 @@
+str = " python"
+
+"'in the given string the index are as shown"
+"'  p   y   t  h   o  n'"
+"'  0   1   2   3  4  5 where 0 is the starting index and 5 is the ending index "
+"'-6  - 5  -4  -3 -2 -1 where -1 is the index from the end"
