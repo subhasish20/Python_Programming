e66ca3f878a3e7589d51204f27f843bf64588c34
Subhasish Jena <subhasishjena8280@gmail.com>
2026-09-25 17:09:36 +0530

    area-of-(questions)

---
+++ area-queston/area-of-cicle2.py
@@ -0,0 +1,14 @@
+#by using math module in python
+import math
+
+#taking input from the the user
+radius = int ( input ('enter the radius of the circle :'))
+
+pi = 3.141 #declaring the value of pi
+
+#calculating the area of the circle
+area = pi * pow (radius,2)
+
+#printing the area of the circle
+print("The area of the circle is :",area)
+
---
+++ area-queston/area-of-the-square2.py
@@ -0,0 +1,3 @@
+import math as mt 
+a = int ( input ("enter the value of the side :"))
+print("Area of the square is :", mt.pow(a,2))
